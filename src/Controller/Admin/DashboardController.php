854acd000736674baafdad375e9ed33b3c506de4
Michael Ballester <mike@mikebgdev.com>
2023-03-15 19:10:01 +0100

    add easyadmin tablas projecto y skills

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,44 +2,75 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Project;
+use App\Entity\Skill;
+use Doctrine\Persistence\ManagerRegistry;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
+    private ChartBuilderInterface $chartBuilder;
+    private ManagerRegistry $doctrine;
+
+    /**
+     * @param ChartBuilderInterface $chartBuilder
+     */
+    public function __construct(ChartBuilderInterface $chartBuilder, ManagerRegistry $doctrine)
+    {
+        $this->chartBuilder = $chartBuilder;
+        $this->doctrine = $doctrine;
+    }
+
+
     public function index(): Response
     {
-        return parent::index();
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_BAR);
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $repository = $this->doctrine->getRepository(Skill::class);
+        $countSkills = $repository->count([]);
+        $repository = $this->doctrine->getRepository(Project::class);
+        $countProjects = $repository->count([]);
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
+        $chart->setData([
+            'labels' => ['My Stats'],
+            'datasets' => [
+                [
+                    'label' =>'Skills',
+                    'borderColor' => 'green',
+                    'backgroundColor' => 'green',
+                    'data' =>  [$countSkills]
+                ],
+                [
+                    'label' =>'Projects',
+                    'borderColor' => 'blue',
+                    'backgroundColor' => 'blue',
+                    'data' =>  [$countProjects]
+                ],
+            ],
+        ]);
 
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('admin/my-dashboard.html.twig', [
+            'chart' => $chart,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Portfolio Symfony');
+            ->setTitle('Portfolio Symfony')
+            ->setFaviconPath('icon-dev-logo.png');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Projects', 'fas fa-desktop', Project::class);
+        yield MenuItem::linkToCrud('Skills', 'fas fa-skill', Skill::class);
     }
 }
