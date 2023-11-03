513bf608dc6f2f5296541ee2bc53bbd1e73d5b94
Michael Ballester <mike@mikebgdev.com>
2023-11-03 14:23:37 +0100

    update edidorconfig anf php fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,15 @@
 <?php
+/*
+ * This class is part of a software application developed by Michael Ballester Granero.
+ *
+ * The application is distributed under the terms of the MIT License.
+ * For more information, please see the LICENSE file included in the source code.
+ */
 
 return (new PhpCsFixer\Config())
     ->setUsingCache(false)
     ->setRules([
-        '@PHP71Migration' => true,
+        '@PHP74Migration' => true,
         '@PhpCsFixer' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
@@ -16,13 +22,24 @@ return (new PhpCsFixer\Config())
             'scope' => 'all',
             'strict' => true,
         ],
+        'native_constant_invocation' => [
+            'exclude' => [
+                'null',
+                'false',
+                'true',
+            ],
+            'fix_built_in' => true,
+            'include' => [],
+            'scope' => 'all',
+            'strict' => false,
+        ],
         'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
         'no_unreachable_default_argument_value' => true,
         'comment_to_phpdoc' => true,
         'phpdoc_to_comment' => ['ignored_tags' => ['todo', 'var']],
         'no_superfluous_phpdoc_tags' => false,
         'header_comment' => [
-            'header' => <<< HEADER
+            'header' => <<< 'HEADER'
                 This class is part of a software application developed by Michael Ballester Granero.
 
                 The application is distributed under the terms of the MIT License.
@@ -31,11 +48,12 @@ return (new PhpCsFixer\Config())
             'location' => 'after_open',
             'separate' => 'bottom',
         ],
-        'braces' => ['allow_single_line_closure' => true],
+        'single_line_empty_body' => false,
         'phpdoc_line_span' => ['const' => 'single', 'property' => 'single', 'method' => 'multi'],
         'self_static_accessor' => true,
         'simplified_if_return' => true,
         'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
+        'modernize_strpos' => false,
     ])
     ->setRiskyAllowed(true)
     ->setFinder(
