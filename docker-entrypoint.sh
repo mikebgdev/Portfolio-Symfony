8d41a7ebd715c94c9a930eda4e23a1421c073cd1
Michael Ballester <mike@mikebgdev.com>
2024-03-25 23:06:27 +0100

    change docker-entrypoint copy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
 
+echo "Ejecutando el script docker-entrypoint.sh..."
+echo "LS:"
+ls -l
+
 if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
 if [ -f composer.json ]; then
+	echo "Ejecutando comandos..."
+
 	composer install --prefer-dist --no-progress --no-interaction
 	npm install
 	npm run build
